a329be2df6cc77ae664051bf93649a7df1f0766c
Martin Podlubny <martin.podlubny@gmail.com>
2014-11-15 02:39:31 +0000

    encapsulated receiving message

--- SMS-conversion/src/Playground.java
+++ SMS-conversion/src/Playground.java
@@ -1,4 +1,5 @@
 import com.twilio.sdk.TwilioRestException;
+import com.twilio.sdk.resource.instance.Message;
 
 
 public class Playground {
@@ -8,22 +9,10 @@ public class Playground {
 		// send it to api
 		// ???
 		// receive it on the phone
-		
-		String martin = "+447574155899";
-		String izi = "+447592547625";
-		String anthony = "+447542841422";
-		String atanas = "+447453268653";
-		String sadik = "+447462437621";
-		String vlad = "+447845478600";
-
-		SMSSendingService sss = new SMSSendingService();
-		String text = "This is an awesome message from a culturally-different person in Rwana. Yeeey! It was sent form a Java class.";
-		
-		sss.sendMessage(vlad, text);		
-		sss.sendMessage(izi, text);
-		sss.sendMessage(anthony, text);
-		sss.sendMessage(atanas, text);
-		sss.sendMessage(sadik, text);
+		SMSReceivingService srs = new SMSReceivingService();
+		for (Message m : srs.getInboundMessages()) {
+			System.out.printf("From: %s\nText: %s\n\n", m.getFrom(), m.getBody());
+		}
 	}
 
 }
--- SMS-conversion/src/SMSReceivingService.java
+++ SMS-conversion/src/SMSReceivingService.java
@@ -1,34 +1,29 @@
 //Install the Java helper library from twilio.com/docs/java/install
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
 
 import com.twilio.sdk.*; 
 import com.twilio.sdk.resource.instance.*; 
 import com.twilio.sdk.resource.list.*; 
 
 public class SMSReceivingService {
+	TwilioRestClient client;
+	
+	public SMSReceivingService() {
+		this.client = new TwilioRestClient(TwilioConstants.ACCOUNT_SID, TwilioConstants.AUTH_TOKEN);
+	}
 
-	public static void main(String[] args) throws TwilioRestException {
-		TwilioRestClient client = new TwilioRestClient(
-				TwilioConstants.ACCOUNT_SID, TwilioConstants.AUTH_TOKEN);
-
+	public List<Message> getInboundMessages() throws TwilioRestException {
 		// Build the parameters
-		List<NameValuePair> params = new ArrayList<NameValuePair>();
-		params.add(new BasicNameValuePair("Direction", "inbound"));
-		MessageList messages = client.getAccount().getMessages((Map<String, String>) params);
+		MessageList messages = client.getAccount().getMessages();
+		List<Message> l = new ArrayList<Message>();
 
 		// Loop over messages and print out a property for each one.
 		for (Message message : messages) {
 			if (message.getDirection().equals("inbound")) {
-				System.out.printf("Sid: '%s'\nDirection: '%s'\nFrom: '%s'\nContent: '%s'\n\n",
-								message.getSid(), message.getDirection(),
-								message.getFrom(), message.getBody());
+				l.add(message);
 			}
 		}
-
+		return l;
 	}
 }
