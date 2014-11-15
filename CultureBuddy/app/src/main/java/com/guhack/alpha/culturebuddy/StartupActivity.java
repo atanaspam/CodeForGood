507f7c67eaedd3e771642015313d215a8ac68d8b
vlad92z <zaharovs92v@gmail.com>
2014-11-15 00:05:29 +0000

    inital version of the app

---
+++ CultureBuddy/app/src/main/java/com/guhack/alpha/culturebuddy/StartupActivity.java
@@ -0,0 +1,47 @@
+package com.guhack.alpha.culturebuddy;
+
+import android.graphics.Typeface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class StartupActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+        android.support.v7.app.ActionBar actionbar = getSupportActionBar();
+        if (actionbar != null) actionbar.hide();
+
+        TextView myTextView=(TextView)findViewById(R.id.title);
+        Typeface typeFace=Typeface.createFromAsset(getAssets(),"fontmain.ttf");
+        myTextView.setTypeface(typeFace);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_startup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
